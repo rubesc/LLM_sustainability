8241269c6909b44d565c7367d0a3977a690a6077
rubesc <ruby.w.chu@gmail.com>
2023-12-03 22:11:37 -0500

    updated

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
-// In your server.js or index.js file
-
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const axios = require('axios');
 
-app.use(bodyParser.json());
+const app = express();
+const port = 3000;
+
+// Body parsing middleware
+app.use(express.json());
 
 // Define a route for your API endpoint
-app.post('/generate-recommendations', (req, res) => {
-  // Handle GPT-3 API request here
-  const locale = req.body.locale;
-  // Call GPT-3 API with the 'locale' parameter and generate recommendations
-  // ...
+app.post('/generate-recommendations', async (req, res) => {
+  try {
+    // Handle GPT-3 API request here
+    const locale = req.body.locale;
 
-  // Send the recommendations as the response
-  res.json({ recommendations: /* your generated recommendations */ });
+    // Call GPT-3 API with the 'locale' parameter and generate recommendations
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: `Act as a climate justice activist/expert. Generate 20 actionable sustainable initiatives and organizations for a person in ${locale}.`,
+      max_tokens: 150,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer your-api-key', // Replace 'your-api-key' with your actual API key
+      },
+    });
+
+    // Extract recommendations from the GPT-3 API response
+    const recommendations = response.data.choices[0].text;
+
+    // Send the recommendations as the response
+    res.json({ recommendations });
+  } catch (error) {
+    console.error('Error making GPT-3 API request:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
